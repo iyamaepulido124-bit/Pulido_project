7cccb844409cf8df4197ed8a30163a851312e117
magsipo <iyamaepulido124@gmail.com>
2025-10-07 13:50:10 +0800

    added new
    purchased

---
+++ handOn1C.js
@@ -0,0 +1,14 @@
+function checkPurchased(){
+    let Product= document.getElementById 
+    let Price = document.getElementById
+
+    let Quality = document.getElementById
+
+    let Total= document.getElementById
+
+    console.log(checkPurchasedheck);//
+
+    if (PurchasedCheck>=0 && PurchasedCheck<=15){
+console
+    }
+}
